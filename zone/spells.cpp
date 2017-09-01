2a3977197bb94d019b9e0095420f050ce339b590
crdunwel <clayton.dunwell@gmail.com>
2017-09-01 11:14:08 -0400

    casting charm on target with level above max level of spell will give resist message and aggro

--- zone/spells.cpp
+++ zone/spells.cpp
@@ -4418,8 +4418,11 @@ bool Mob::IsImmuneToSpell(uint16 spell_id, Mob *caster)
 			assert(effect_index >= 0);
 			if(GetLevel() > spells[spell_id].max[effect_index] && spells[spell_id].max[effect_index] != 0)
 			{
-				Log(Logs::Detail, Logs::Spells, "Our level (%d) is higher than the limit of this Charm spell (%d)", GetLevel(), spells[spell_id].max[effect_index]);
-				caster->Message_StringID(MT_Shout, CANNOT_CHARM_YET);
+				int32 aggro = caster->CheckAggroAmount(spell_id, this);
+				aggro > 0 ? AddToHateList(caster, aggro) : AddToHateList(caster, 1, 0, true, false, false, spell_id);
+				caster->Message_StringID(MT_SpellFailure, TARGET_RESISTED, spells[spell_id].name);
+				// Log(Logs::Detail, Logs::Spells, "Our level (%d) is higher than the limit of this Charm spell (%d)", GetLevel(), spells[spell_id].max[effect_index]);
+				// caster->Message_StringID(MT_Shout, CANNOT_CHARM_YET);
 				return true;
 			}
 		}
