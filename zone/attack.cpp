b4e69a4a258496779d078df3aca0ea8dad5b83ab
Michael Cook (mackal) <mcook@mackal.net>
2018-03-01 13:41:02 -0500

    Use the melee push pos update timer for rate limiting the pushes
    Also lowered the timer since it's limiting actual movement now
    
    Conflicts:
    	zone/mob.cpp

--- zone/attack.cpp
+++ zone/attack.cpp
@@ -3644,16 +3644,14 @@ void Mob::CommonDamage(Mob* attacker, int &damage, const uint16 spell_id, const 
 			// update NPC stuff
 			auto new_pos = glm::vec3(m_Position.x + (a->force * std::cos(a->hit_heading) + m_Delta.x),
 				m_Position.y + (a->force * std::sin(a->hit_heading) + m_Delta.y), m_Position.z);
-			if (zone->zonemap && zone->zonemap->CheckLoS(glm::vec3(m_Position), new_pos)) { // If we have LoS on the new loc it should be reachable.
+			if (position_update_melee_push_timer.Check() && zone->zonemap && zone->zonemap->CheckLoS(glm::vec3(m_Position), new_pos)) { // If we have LoS on the new loc it should be reachable.
 				if (IsNPC()) {
 					// Is this adequate?
 					//float size_mod = GetSize() / RuleR(Combat, MeleePushSizeMod);
 					//new_pos.x -= std::sin(a->meleepush_xy);
 					//new_pos.y -= std::cos(a->meleepush_xy);
 					Teleport(new_pos);
-					if (position_update_melee_push_timer.Check()) {
-						SendPositionUpdate();
-					}
+					SendPositionUpdate();
 				}
 			}
 			else {
