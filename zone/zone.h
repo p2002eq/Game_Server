d45b9d76828a67d6a72a6898070a31429e15c1d7
Trust <fryguy503@gmail.com>
2018-04-18 17:56:36 -0400

    [General] Added #reloadmerchants for zone-based merchant refreshing.

--- zone/zone.h
+++ zone/zone.h
@@ -211,6 +211,7 @@ public:
 	void	LoadAlternateCurrencies();
 	void	LoadNPCEmotes(LinkedList<NPC_Emote_Struct*>* NPCEmoteList);
 	void	ReloadWorld(uint32 Option);
+	void	ReloadMerchants();
 
 	Map*	zonemap;
 	WaterMap* watermap;
