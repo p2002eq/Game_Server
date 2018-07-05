6c9c16e1d2a377704ca60e2cbed604cf54444c0e
Scott Davis <me@sdavis.info>
2018-07-05 00:47:08 -0400

    Revert "[Spells] Fix for bug #684 Calm/memblur that are eligible can now be hasted."
    This reverts commit 0b46bd83005434273b42110fb0d64ad5446ca3ca.

--- common/spdat.cpp
+++ common/spdat.cpp
@@ -221,7 +221,6 @@ bool IsGroupOnlySpell(uint16 spell_id)
 bool IsBeneficialSpell(uint16 spell_id) {
 	if (!IsValidSpell(spell_id)) {
 		return false;
-	}
 
 	// You'd think just checking goodEffect flag would be enough?
 	if (spells[spell_id].goodEffect == 1) {
@@ -231,6 +230,32 @@ bool IsBeneficialSpell(uint16 spell_id) {
 		if (tt != ST_Self && tt != ST_Pet &&
 			IsEffectInSpell(spell_id, SE_CancelMagic)) {
 			return false;
+
+		// When our targettype is ST_Target, ST_AETarget, ST_Aniaml, ST_Undead, or ST_Pet
+		// We need to check more things!
+		if (tt == ST_Target || tt == ST_AETarget || tt == ST_Animal ||
+			tt == ST_Undead || tt == ST_Pet) {
+			uint16 sai = spells[spell_id].SpellAffectIndex;
+
+			// If the resisttype is magic and SpellAffectIndex is Calm/memblur/dispell sight
+			// it's not beneficial
+			if (spells[spell_id].resisttype == RESIST_MAGIC) {
+				// checking these SAI cause issues with the rng defensive proc line
+				// So I guess instead of fixing it for real, just a quick hack :P
+				if (spells[spell_id].effectid[0] != SE_DefensiveProc &&
+					(sai == SAI_Calm || sai == SAI_Dispell_Sight || sai == SAI_Memory_Blur ||
+					 sai == SAI_Calm_Song))
+					return false;
+			} else {
+				// If the resisttype is not magic and spell is Bind Sight or Cast Sight or Harmony
+				// It's not beneficial
+				if (sai == SAI_Calm && IsEffectInSpell(spell_id, SE_Harmony)
+					|| (sai == SAI_Calm_Song && IsEffectInSpell(spell_id, SE_BindSight))
+					|| (sai == SAI_Dispell_Sight && spells[spell_id].skill == 18 &&
+						!IsEffectInSpell(spell_id, SE_VoiceGraft))
+						)
+					return false;
+			}
 		}
 	}
 
