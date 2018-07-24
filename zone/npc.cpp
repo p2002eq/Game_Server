a7493f9d1d6ef7ee498afc08a4a3349185d197a0
Scott Davis <me@sdavis.info>
2018-07-24 13:01:50 -0400

    NPCs will no longer turn and face you if turning in items while sneaking [Upstream]

--- zone/npc.cpp
+++ zone/npc.cpp
@@ -2671,9 +2671,13 @@ uint32 NPC::GetSpawnKillCount()
 void NPC::DoQuestPause(Mob *other) {
 	if(IsMoving() && !IsOnHatelist(other)) {
 		PauseWandering(RuleI(NPC, SayPauseTimeInSec));
-		FaceTarget(other);
+		if(other && !other->sneaking) {
+			FaceTarget(other);
+		}
 	} else if(!IsMoving()) {
-		FaceTarget(other);
+		if (other && !other->sneaking && GetAppearance() != eaSitting && GetAppearance() != eaDead) {
+			FaceTarget(other);
+		}
 	}
 
 }
